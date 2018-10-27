8cbea5d57437ada3b7c080fb2b71991bd9685936
Michael Czaja <futureapplications@gmail.com>
2018-10-27 18:10:47 +0200

    lobob

--- script.sh
+++ script.sh
@@ -1,23 +1,52 @@
 #!/bin/sh
+cwd=$(pwd)
+
+
+#goal_build_run_jump <gjb> 
+hot() {
+  echo "Running function hot()"
+  jumpLocation="docker/peel-framework-ubuntu/"
+  cd $jumpLocation
+  docker build -t mtest .
+  docker run -it mtest bash
+  cd $cwd
+}
 
 option="${1}" 
 case ${option} in 
     # Stops and wipes all containers
+    "hot")
+    echo $cwd "+++2"
+    hot
+    ;;
     "clean_cons") 
       echo "Cleaning all docker-containers..."
       docker stop $(docker ps -aq)
       docker rm $(docker ps -aq)
-      ;;
+    ;;
+    "me")
+      echo "hello"
+    ;;
     "clean") 
       echo "File name is ODD"
-      
-      ;;  
-   *)  
+    ;;  
+    "bpfu") 
+      echo "(╯°□°)╯ bpfu is running.#. "
+      docker build -t mtest -f "./docker/peel-framework-ubuntu/Dockerfile" .
+    ;; 
+    *)  
+      # Prints the default usage with explains which parameters could be used
       echo "`basename ${0}`:"
-      echo "usage: 
+      echo "Don't understand you ¯\_(ツ)_/¯ --- Check the usage-section:"
+      echo " 
       PARAM 1:  
-        clean_cons |  Stops all running containers and removes all containers from the system."
+        clean_cons |  Stops all running containers and removes all containers from the system.
+        bpfu       |  Builds the dockerfile from ./docker/peel-framework-ubuntu/Dockerfile"
+      
 
       exit 1 # Command to come out of the program with status 1
       ;; 
 esac 
+
+
+  
