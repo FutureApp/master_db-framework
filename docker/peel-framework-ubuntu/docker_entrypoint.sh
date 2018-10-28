88fe4ec113697fe151da281d9007664eca8f4c75
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2018-10-28 18:36:35 +0100

    inter push

--- docker/peel-framework-ubuntu/docker_entrypoint.sh
+++ docker/peel-framework-ubuntu/docker_entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 cwd=$(pwd)
 
+# Runs the peel-wordcount-standard test
+echo "Running the wordcount.default test-suite from the peel-framework"
+cd "$BUNDLE_BIN/$BUNDLE_AID"; ./peel.sh suite:run wordcount.default
+
